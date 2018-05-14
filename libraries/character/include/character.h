f6edbdd39ffde03c72ce45769b6b2859c4fe91a9
Marianne Tromp <mtromp@mtromp.com>
2018-05-14 19:08:18 -0400

    Made gmock/gtest easier to use. Failing test

---
+++ libraries/character/include/character.h
@@ -0,0 +1,11 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+
+class character
+{
+public:
+    character();
+};
+
+#endif // CHARACTER_H
---
+++ libraries/character/tests/charactertest.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(characterTest, characterNameCanBeSet)
+{
+    EXPECT_FALSE(true);
+}
